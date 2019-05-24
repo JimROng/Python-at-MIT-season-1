ee007366adcf5808611acec4e1a7c847a215a987
JimROng <37008740+JimROng@users.noreply.github.com>
2019-05-24 16:53:57 +0800

    Add files via upload

---
+++ notebook mit p1.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 23 17:18:27 2019
+
+@author: lijiu
+"""
+print("1.1")
+#represent knowledge with data structure
+#iteration and recursion
+#abstraction 
+#orginize and modularize
+#algorithms
+#computational thinking 
+
+#Fundenmentally, computer do calculations; remember results
+#what kinds of calculation 
+#algorithm 
+
+#limitations
+print("1.2/1.3")
+print("declarative knowledge : statements of fact ")
+print("imperative knowledge: a recipe of how")
+print("simple steps - flow of control - when to stop =algorithm") 
+print("computer: - mechanic problem -fixed program(computer)")
+
+print(
+      "input="
+      "basic machine architecture: memory(data)" 
+      "-control unit(program counter)" 
+      "-arithmetic logic unit(do primitive ops)"
+      "=output"
+     )
+#sequence of instruction stored 
+#-arithmetic and logic
+#-simple tests
+#-moving data
+#special program executes each instruction in order and stop when done
+print("From Turing:"
+      "compute anything using 6 primitives"
+      "create new primitives"
+      "Turing complete: one language is computable in any other programming language"
+      "highlight:abstract"
+        )
+
+print("1.4"
+      "operations and expressions"
+      "values"
+      "primitive construct"
+      "remember syntax and semantics"
+      )
+print("static semantics"
+      "express the meaning"
+      "CS language has only one meaning")
+
+print("1.5"
+      "shell"
+      "program with sequence and commands with values"
+      "store in the shell and directly type"
+      )
+#int / float / bool / nonetype / type(see an object)
+#float() / int()
+#print is a non type
+#expressions: <object><operator><object>
+#operation tend to be higher
+#//int division % remains i**j power 
+# = is assignment varable = value
